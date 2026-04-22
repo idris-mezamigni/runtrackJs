985aa81154c270439725df870bbf751ee05c6249
idris-mezamigni <idris.mezamigni@laplateforme.io>
2026-04-22 15:29:07 +0200

    Jour 03 : Jobs 01 et 02

---
+++ jour03/job01/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    
+    // 1. Quand on clique sur le bouton "Afficher"
+    $('#show').click(function() {
+        // On affiche l'élément qui a l'id 'citation'
+        $('#citation').show();
+    });
+
+    // 2. Quand on clique sur le bouton "Cacher"
+    $('#hide').click(function() {
+        // On cache l'élément qui a l'id 'citation'
+        $('#citation').hide();
+    });Rename-Item jour3 jour03
+
+});
